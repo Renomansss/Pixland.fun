20e173e855f2dd483c7a332c5de437df2af574d6
Renomansss <152803547+Renomansss@users.noreply.github.com>
2024-04-20 19:00:41 +0300

    Create News.jsx

---
+++ src/components/News.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsPage = () => {
+  return (
+    <div style={{ fontWeight: 'bold', fontSize: '1.2em' }}>
+      Very soon...
+    </div>
+  );
+};
+
+export default News;
